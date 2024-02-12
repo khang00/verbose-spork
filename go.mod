41ef2835e77108798590b2a0bb22a71d26b128f4
khangdang <dlqkhang8@gmail.com>
2024-02-12 17:27:18 +0700

    feats: add JWTs

--- go.mod
+++ go.mod
@@ -14,6 +14,7 @@ require (
 	github.com/antchfx/xmlquery v1.3.18 // indirect
 	github.com/antchfx/xpath v1.2.4 // indirect
 	github.com/gobwas/glob v0.2.3 // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
---
+++ internal/pkg/jwt/jwt.go
@@ -0,0 +1,31 @@
+package jwt
+
+import (
+	"fmt"
+	"github.com/golang-jwt/jwt"
+)
+
+var defaultSecretKey = []byte("secret")
+
+func CreateToken(key string, val string) (string, error) {
+	token := jwt.New(jwt.SigningMethodHS256)
+	claims := token.Claims.(jwt.MapClaims)
+	claims[key] = val
+
+	return token.SignedString(defaultSecretKey)
+}
+
+func ValidateToken(jwtString string) (*jwt.Token, error) {
+	return jwt.Parse(jwtString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("there's an error with the signing method")
+		}
+
+		return defaultSecretKey, nil
+	})
+}
+
+func GetClaims(token *jwt.Token) (jwt.MapClaims, bool) {
+	claims, ok := token.Claims.(jwt.MapClaims)
+	return claims, ok
+}
