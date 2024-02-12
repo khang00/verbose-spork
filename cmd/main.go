7e66a6d1ea4bf0c88c542a0a3d48fb27ecfe09c1
khangdang <dlqkhang8@gmail.com>
2024-02-12 16:09:14 +0700

    feats: add auth APIs

--- cmd/main.go
+++ cmd/main.go
@@ -3,20 +3,30 @@ package main
 import (
 	"fmt"
 	"github.com/khang00/verbose-spork/internal/handler"
+	"github.com/khang00/verbose-spork/internal/store"
 	"net/http"
 )
 
 func main() {
-	setupHandler()
-
-	err := http.ListenAndServe(":8080", nil)
+	db, err := store.NewPostgresStore(nil)
 	if err != nil {
 		fmt.Println(err)
-	} else {
-		fmt.Println("Server listening on port 8080")
+	}
+
+	authService := handler.NewAuthHandler(db)
+
+	setupHandler(authService)
+
+	fmt.Println("Server listening on port 8080")
+	err = http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
 }
 
-func setupHandler() {
+func setupHandler(auth *handler.AuthHandler) {
 	http.HandleFunc("/health", handler.HealthHandler)
+	http.HandleFunc("/api/user/signup", auth.Signup)
+	http.HandleFunc("/api/user/signin", auth.Signin)
 }
